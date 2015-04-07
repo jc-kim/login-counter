f6c0e1f1278b687cbeaff681acddc0d3e168f24c
Kim, JaeChan <kjc0210@gmail.com>
2015-04-07 10:46:10 -0400

    implement signup method

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,15 @@
 class UserController < ApplicationController
   def signup
+    user = User.new(username: params[:username],
+                    password: params[:password])
+    if not(user.valid?)
+      return render json: {"error_code" => user.error_code}
+    end
+
+    user.login_count = 1
+    user.save
+    render json: {"user_name" => user.username,
+                  "login_count" => user.login_count}
   end
 
   def login
--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,9 +1,54 @@
 require 'test_helper'
 
 class UserControllerTest < ActionController::TestCase
-  test "should get signup" do
-    get :signup
-    assert_response :success
+  test "should success signup" do
+    assert_difference('User.count') do
+      post :signup, {"username" => "new_user",
+                     "password" => "valid_password"},
+                     format: "json"
+      assert_response :success
+      body = JSON.parse(response.body)
+      assert body["user_name"] == "new_user"
+      assert body["login_count"] == 1
+    end
+
+    assert_no_difference('User.count') do
+      post :signup, {"username" => "new_user",
+                     "password" => "valid_password"},
+                     format: "json"
+      assert_response :success
+      body = JSON.parse(response.body)
+      assert body["error_code"] = -3  # duplicate username
+    end
+  end
+
+  test "should failed signup" do
+    assert_no_difference('User.count') do
+      post :signup, {"username" => "too_long_username_to_signup_so_must_failed",
+                     "password" => "valid_password"},
+                     format: "json"
+      assert_response :success
+      body = JSON.parse(response.body)
+      assert body["error_code"] = -1  # username length is execeed to limit
+    end
+
+    assert_no_difference('User.count') do
+      post :signup, {"username" => "new_user",
+                     "password" => "too_long_pasword_to_signup_so_must_failed"},
+                     format: "json"
+      assert_response :success
+      body = JSON.parse(response.body)
+      assert body["error_code"] = -2  # password length is execeed to limit
+    end
+
+    assert_no_difference('User.count') do
+      post :signup, {"username" => "user1",
+                     "password" => "valid_password"},
+                     format: "json"
+      assert_response :success
+      body = JSON.parse(response.body)
+      assert body["error_code"] = -3  # duplicate username
+    end
   end
 
   test "should get login" do
