1807c6f379caf718cde52eb429bccfef1d8096f4
Kim, JaeChan <kjc0210@gmail.com>
2015-04-07 10:03:59 -0400

    add user model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,35 @@
 class User < ActiveRecord::Base
+  validate :username_validate, :password_validate
+
+  def username_validate
+    if username.nil?
+      errors.add(:username, {code: -1})
+      return
+    end
+    unless 5 <= username.length and username.length <= 20
+      errors.add(:username, {code: -1})
+      return
+    end
+    unless User.where(username: username).count == 0
+      errors.add(:username, {code: -3})
+      return
+    end
+  end
+
+  def password_validate
+    if password.nil?
+      errors.add(:password, {code: -2})
+      return
+    end
+    unless 5 <= password.length and password.length <= 20
+      errors.add(:password, {code: -2})
+      return
+    end
+  end
+
+  def error_code
+    return errors[:username][0][:code] if errors[:username].length > 0
+    return errors[:password][0][:code] if errors[:password].length > 0
+    return nil
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "username length has limit" do
+    user = User.new(username: "valid_username", password: "valid_password")
+    assert user.valid?
+
+    user1 = User.new(username: "shrt", password: "valid_password")
+    assert not(user1.valid?)
+    assert user1.error_code == -1
+
+    user2 = User.new(username: "that username is too long so it's invalid", password: "valid_password")
+    assert not(user1.valid?)
+    assert user1.error_code == -1
+  end
+
+  test "username should be unique" do
+    user1 = User.new(username: "user1", password: "valid_password")
+    assert not(user1.valid?)
+    assert user1.error_code == -3
+  end
+
+  test "password length has limit" do
+    user = User.new(username: "validuser", "password": "inva")
+    assert not(user.valid?)
+    assert user.error_code == -2
+  end
 end
