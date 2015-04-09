dab63b4d77ef558cf7c661eb993a928b7b9f13b5
Kim, JaeChan <kjc0210@gmail.com>
2015-04-09 03:50:38 -0400

    allow other domain to access via ajax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   skip_before_filter :verify_authenticity_token
+
+  after_filter :cors_set_response_header
+
+  def cors_set_response_header
+    headers['Access-Control-Allow-Origin'] = '*'
+  end
 end
