762c713908f8c3b4d399e84009dd1073590d5bf1
Kim, JaeChan <kjc0210@gmail.com>
2015-04-09 05:52:18 -0400

    prepare to deploy on nginx

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,11 +25,13 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
+  config.assets.compress = true
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :yui
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.precompile = ['*.js', '*.css', '*.css.erb']
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
